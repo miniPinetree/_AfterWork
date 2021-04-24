ece560cead2bdb41c5fc8094995a02e0f8ea5bb9
go <misong321@gmail.com>
2021-04-24 14:15:12 +0900

    [chore]:Set initial environment

---
+++ src/pages/Test.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Test=(props)=>{
+    return(
+<div>test</div>
+    );
+}
+export default Test;
---
+++ src/shared/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ConnectedRouter } from "connected-react-router";
+import { Route } from "react-router-dom";
+import './App.css';
+import Test from "../pages/Test";
+
+function App() {
+  return (
+    <React.Fragment>
+    <div>test</div>
+    </React.Fragment>
+  );
+}
+export default App;
