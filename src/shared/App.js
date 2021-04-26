1d46700fe5d711d1a1ccc767b1c3a979fe61bf54
noh-yj <goky456@gmail.com>
2021-04-26 10:24:25 +0900

    [chore]: remove Test.js

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,14 +3,11 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import { history } from '../redux/configStore';
-import Test from '../pages/Test';
 
 function App() {
   return (
     <>
-      <ConnectedRouter history={history}>
-        <Route path='/test' component={Test} />
-      </ConnectedRouter>
+      <ConnectedRouter history={history}></ConnectedRouter>
       good
     </>
   );
