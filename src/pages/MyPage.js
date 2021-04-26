2863759149a40eb344b895deb846693a215feb52
go <misong321@gmail.com>
2021-04-26 14:26:12 +0900

    [feat]:Make mypage

---
+++ src/pages/MyPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MyPage=(props)=>{
+    return(
+        <div>mypage</div>
+    );
+}
+export default MyPage;
--- src/shared/App.js
+++ src/shared/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import { history } from '../redux/configStore';
 import Test from '../pages/Test';
+import {MyPage} from "../pages";
 
 function App() {
   return (
     <>
       <ConnectedRouter history={history}>
         <Route path='/test' component={Test} />
+        <Route path='/mypage' component={MyPage}/>
       </ConnectedRouter>
       good
     </>
