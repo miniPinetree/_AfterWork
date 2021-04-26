e15f7b78a4116e1dbe5515ec123910d2ab3f97bf
noh-yj <goky456@gmail.com>
2021-04-26 16:31:26 +0900

    [chor]: add to main

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import { history } from '../redux/configStore';
+import Main from '../pages/Main';
 
 function App() {
   return (
     <>
-      <ConnectedRouter history={history}></ConnectedRouter>
-      good
+      <ConnectedRouter history={history}>
+        <Route path='/' exact component={Main} />
+      </ConnectedRouter>
     </>
   );
 }
