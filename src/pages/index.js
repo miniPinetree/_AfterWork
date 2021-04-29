849ad6047ffa5ffcf0081efc300185e6f11cbbed
illy <ma_natsu03@naver.com>
2021-04-29 21:53:07 +0900

    [add] index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
-import MyPage from './MyPage';
-import Main from './Main';
-import Category from './Category';
-import UserDetail from './UserDetail';
-import Search from './Search';
+import MyPage from "./MyPage";
+import Main from "./Main";
+import Category from "./Category";
+import UserDetail from "./UserDetail";
+import Search from "./Search";
+import About from "./About";
 
-export { MyPage, Main, UserDetail, Category, Search };
+export { MyPage, Main, UserDetail, Category, Search, About };
