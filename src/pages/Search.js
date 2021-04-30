8b170f177dcd7b1df0b094206ff6b60c647b0446
noh-yj <goky456@gmail.com>
2021-04-30 14:05:06 +0900

    [feat]: add to search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import queryString from 'query-string';
 import styled from 'styled-components';
 import { Select } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { actionCreators as postActions } from '../redux/modules/post';
+import InfinityScroll from '../shared/InfinityScroll';
 import SubBanner from '../components/SubBanner';
-import ItemCard from '../components/ItemCard';
+import PostCard from '../components/PostCard';
 import SideBar from '../components/SideBar';
 
 function Search(props) {
+  const dispatch = useDispatch();
   const query = queryString.parse(props.location.search);
   const { keyword } = query;
+  const [filterBox, setfilterBox] = useState('total');
+  const [sortBox, setSortBox] = useState('popularity desc');
+  let post_list = useSelector((state) => state.post.post_list);
+  const paging = useSelector((state) => state.post.paging);
+  const is_loading = useSelector((state) => state.post.is_loading);
+  const view_loading = useSelector((state) => state.post.view_loading);
+
+  const sortInfo = sortBox.split(' ');
+  const sort = sortInfo[0];
+  const direction = sortInfo[1];
+
+  useEffect(() => {
+    dispatch(postActions.getSearchDB(keyword, sort, direction));
+  }, [direction, dispatch, keyword, sort]);
+
   const { Option } = Select;
 
-  const handleChange = (value) => {
-    console.log(value);
+  const selectSort = (value) => {
+    setSortBox(value);
   };
+  const selectFilter = (value) => {
+    setfilterBox(value);
+  };
+  if (filterBox === 'online') {
+    post_list = post_list.filter((val) => {
+      return val.online === true;
+    });
+  } else if (filterBox === 'offline') {
+    post_list = post_list.filter((val) => {
+      return val.online === false;
+    });
+  }
 
   return (
     <>
@@ -23,52 +54,51 @@ function Search(props) {
           <Container>
             <SideBar />
             <MainContainer>
-              <MainHeader>
-                <div>'{keyword}' 검색 결과 0건 </div>
+              {!view_loading ? (
+                <>
+                  <MainHeader>
+                    <div>
+                      '{keyword}' 검색 결과 {paging.total}건{' '}
+                    </div>
 
-                <div>
-                  <Select
-                    defaultValue='인기순'
-                    style={{ width: 112, marginRight: '15px' }}
-                    onChange={handleChange}
-                  >
-                    <Option value='인기순'>인기순</Option>
-                    <Option value='가격 낮은순'>가격 낮은순</Option>
-                    <Option value='가격 높은순'>가격 높은순</Option>
-                  </Select>
-                  <Select
-                    defaultValue='전체보기'
-                    style={{ width: 94 }}
-                    onChange={handleChange}
-                  >
-                    <Option value='전체보기'>전체보기</Option>
-                    <Option value='온라인'>온라인</Option>
-                    <Option value='오프라인'>오프라인</Option>
-                  </Select>
-                </div>
-              </MainHeader>
-              <Main>
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-              </Main>
+                    <div>
+                      <Select
+                        defaultValue='popularity desc'
+                        style={{ width: 112, marginRight: '15px' }}
+                        onChange={selectSort}
+                        value={sortBox}
+                      >
+                        <Option value='popularity desc'>인기순</Option>
+                        <Option value='price asc'>가격 낮은순</Option>
+                        <Option value='price desc'>가격 높은순</Option>
+                      </Select>
+                      <Select
+                        defaultValue='total'
+                        style={{ width: 94 }}
+                        onChange={selectFilter}
+                        value={filterBox}
+                      >
+                        <Option value='total'>전체보기</Option>
+                        <Option value='online'>온라인</Option>
+                        <Option value='offline'>오프라인</Option>
+                      </Select>
+                    </div>
+                  </MainHeader>
+                  <Main>
+                    <InfinityScroll
+                      callNext={() => {
+                        dispatch(postActions.scrollSearchDB());
+                      }}
+                      is_next={paging.page ? true : false}
+                      loading={is_loading}
+                    >
+                      {post_list.map((val, idx) => {
+                        return <PostCard post_info={val} key={idx} />;
+                      })}
+                    </InfinityScroll>
+                  </Main>
+                </>
+              ) : null}
             </MainContainer>
           </Container>
         </>
