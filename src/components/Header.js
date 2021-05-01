43644a1291f3f3d0c6f08a54eb8b92d92b20b263
illy <ma_natsu03@naver.com>
2021-05-01 15:04:36 +0900

    [fix] if logout, move to main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,64 +1,63 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import styled from 'styled-components';
-import { history } from '../redux/configStore';
-import LoginModal from './LoginModal';
-import { actionCreators as userActions } from '../redux/modules/user';
-import { hamburger } from '../shared/images/text-align-justified.svg';
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
+import { history } from "../redux/configStore";
+import LoginModal from "./LoginModal";
+import { actionCreators as userActions } from "../redux/modules/user";
+import { hamburger } from "../shared/images/text-align-justified.svg";
 
 const Header = () => {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user.user);
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user.user);
 
-  const [isModal, setIsModal] = useState(false);
-  const modalOpen = () => {
-    setIsModal(true);
-  };
-  const modalClose = () => {
-    setIsModal(false);
-  };
+    const [isModal, setIsModal] = useState(false);
+    const modalOpen = () => {
+        setIsModal(true);
+    };
+    const modalClose = () => {
+        setIsModal(false);
+    };
 
-  return (
-    <>
-      <Wrap>
-        <Body>
-          <div>
-            <Logo
-              onClick={() => {
-                history.push('/');
-              }}
-            >
-              퇴근하고 뭐하지?
-            </Logo>
-            <About onClick={() => history.push('/about')}>About</About>
-          </div>
-          <div>
-            {user ? (
-              <>
-                <Login
-                  onClick={() => {
-                    dispatch(userActions.logOut());
-                  }}
-                >
-                  로그아웃
-                </Login>
+    return (
+        <>
+            <Wrap>
+                <Body>
+                    <div>
+                        <Logo
+                            onClick={() => {
+                                history.push("/");
+                            }}
+                        >
+                            퇴근하고 뭐하지?
+                        </Logo>
+                        <About onClick={() => history.push("/about")}>About</About>
+                    </div>
+                    <div>
+                        {user ? (
+                            <>
+                                <Login
+                                    onClick={() => {
+                                        dispatch(userActions.logOut());
+                                    }}
+                                >
+                                    로그아웃
+                                </Login>
 
-                <Login onClick={() => history.push('/mypage')}>
-                  {user.name}님
-                </Login>
-              </>
-            ) : (
-              <Login onClick={modalOpen}>로그인</Login>
-            )}
-            {isModal === true ? <LoginModal close={modalClose} /> : null}
-          </div>
-        </Body>
-      </Wrap>
-    </>
-  );
+                                <Login onClick={() => history.push("/mypage")}>{user.name}님</Login>
+                            </>
+                        ) : (
+                            <Login onClick={modalOpen}>로그인</Login>
+                        )}
+                        {isModal === true ? <LoginModal close={modalClose} /> : null}
+                    </div>
+                </Body>
+            </Wrap>
+        </>
+    );
 };
 
 const Wrap = styled.div`
+<<<<<<< Updated upstream
   width: 100%;
   height: 100%;
   border-bottom: 1px solid #707070;
@@ -81,40 +80,40 @@ const Wrap = styled.div`
 `;
 
 const Body = styled.div`
-  max-width: 1004px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 0 auto;
-  @media all and (max-width: 375px) {
-    display: block;
-    text-align: center;
-    margin: 0;
-  }
+    max-width: 1004px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    @media all and (max-width: 375px) {
+        display: block;
+        text-align: center;
+        margin: 0;
+    }
 `;
 
 const Logo = styled.span`
-  font-size: 20px;
-  font-weight: bold;
-  letter-spacing: -0.6px;
-  cursor: pointer;
-  height: 100%;
-  margin-right: 24px;
-  @media all and (max-width: 375px) {
-    margin: 0 auto;
-    text-align: center;
-  }
+    font-size: 20px;
+    font-weight: bold;
+    letter-spacing: -0.6px;
+    cursor: pointer;
+    height: 100%;
+    margin-right: 24px;
+    @media all and (max-width: 375px) {
+        margin: 0 auto;
+        text-align: center;
+    }
 `;
 
 const About = styled.span`
-  font-size: 16px;
-  font-weight: normal;
-  letter-spacing: -0.6px;
-  cursor: pointer;
-  height: 100%;
-  @media all and (max-width: 375px) {
-    display: none;
-  }
+    font-size: 16px;
+    font-weight: normal;
+    letter-spacing: -0.6px;
+    cursor: pointer;
+    height: 100%;
+    @media all and (max-width: 375px) {
+        display: none;
+    }
 `;
 
 const Login = styled.span`
@@ -126,6 +125,49 @@ const Login = styled.span`
   @media all and (max-width: 375px) {
     display: none;
   }
+=======
+    width: 100%;
+    height: 100%;
+    border-bottom: 1px solid #707070;
+    position: sticky;
+    top: 0px;
+    padding: 14px 0;
+    background: #fff;
+    z-index: 3;
+`;
+
+const Body = styled.div`
+    max-width: 1004px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+`;
+
+const Logo = styled.span`
+    font-size: 20px;
+    font-weight: bold;
+    letter-spacing: -0.6px;
+    cursor: pointer;
+    height: 100%;
+    margin-right: 24px;
+`;
+
+const About = styled.span`
+    font-size: 16px;
+    font-weight: normal;
+    letter-spacing: -0.6px;
+    cursor: pointer;
+    height: 100%;
+`;
+
+const Login = styled.span`
+    font-size: 17px;
+    font-weight: normal;
+    cursor: pointer;
+    height: 100%;
+    margin-left: 12px;
+>>>>>>> Stashed changes
 `;
 
 export default Header;
--- src/shared/Cookie.js
+++ src/shared/Cookie.js
@@ -6,7 +6,7 @@ const getCookie = (name) => {
     }
 };
 
-const setCookie = (name, value, exp = 5) => {
+const setCookie = (name, value, exp = 1) => {
     let date = new Date();
     date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
     document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/; secure=true;`;
