08b28c9193909894bd6cca4ab0c9c234f85cf8ac
illy <ma_natsu03@naver.com>
2021-05-04 14:16:28 +0900

    [doc] [fix] change function form

--- src/shared/OAuth2RedirectHandler.js
+++ src/shared/OAuth2RedirectHandler.js
@@ -3,45 +3,91 @@ import { ACCESS_TOKEN } from "./social";
 import { setCookie } from "./Cookie";
 import { Redirect } from "react-router-dom";
 
-class OAuth2RedirectHandler extends Component {
-    getUrlParameter(name) {
+function OAuth2RedirectHandler(props) {
+    const getUrlParameter = (name) => {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
         var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
 
-        var results = regex.exec(this.props.location.search);
+        var results = regex.exec(props.location.search);
         return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-    }
+    };
+    const token = getUrlParameter("token");
+    const error = getUrlParameter("error");
+    console.log(error);
 
-    render() {
-        const token = this.getUrlParameter("token");
-        const error = this.getUrlParameter("error");
-
-        if (token) {
-            setCookie(ACCESS_TOKEN, token);
-            return (
+    if (token) {
+        setCookie(ACCESS_TOKEN, token);
+        return (
+            <Redirect
+                to={{
+                    pathname: "/",
+                    state: { from: props.location },
+                }}
+            />
+        );
+    } else {
+        return (
+            <>
                 <Redirect
                     to={{
                         pathname: "/",
-                        state: { from: this.props.location },
+                        state: {
+                            from: props.location,
+                            error: error,
+                        },
                     }}
                 />
-            );
-        } else {
-            return (
-                <>
-                    <Redirect
-                        to={{
-                            pathname: "/",
-                            state: {
-                                from: this.props.location,
-                                error: error,
-                            },
-                        }}
-                    />
-                </>
-            );
-        }
+            </>
+        );
     }
 }
+// const getUrlParameter = (name) => {
+//     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+//     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+
+//     var results = regex.exec(this.props.location.search);
+//     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+// };
+// class OAuth2RedirectHandler extends Component {
+//     // getUrlParameter(name) {
+//     //     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+//     //     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+
+//     //     var results = regex.exec(this.props.location.search);
+//     //     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+//     // }
+
+//     render() {
+//         const token = this.getUrlParameter("token");
+//         const error = this.getUrlParameter("error");
+//         console.log(error);
+
+//         if (token) {
+//             setCookie(ACCESS_TOKEN, token);
+//             return (
+//                 <Redirect
+//                     to={{
+//                         pathname: "/",
+//                         state: { from: this.props.location },
+//                     }}
+//                 />
+//             );
+//         } else {
+//             return (
+//                 <>
+//                     <Redirect
+//                         to={{
+//                             pathname: "/",
+//                             state: {
+//                                 from: this.props.location,
+//                                 error: error,
+//                             },
+//                         }}
+//                     />
+//                 </>
+//             );
+//         }
+//     }
+// }
 
 export default OAuth2RedirectHandler;
