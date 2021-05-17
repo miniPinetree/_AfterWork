8f34552663db16216327b2a21b1c0213427dcd86
illy <ma_natsu03@naver.com>
2021-05-17 23:25:13 +0900

    [doc] [add] kakao inapp login notice

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,13 +1,13 @@
 import styled, { keyframes } from "styled-components";
 import { GOOGLE_AUTH_URL, KAKAO_AUTH_URL, NAVER_AUTH_URL } from "../shared/social";
+import Swal from "sweetalert2";
 import naver from "../shared/images/naver.png";
 import kakao from "../shared/images/kakao.png";
 import google from "../shared/images/google.png";
 import logo from "../shared/images/login.png";
-import more from "../shared/images/more.svg";
-import share from "../shared/images/share.svg";
 
 const LoginModal = ({ close }, props) => {
+    const ua = navigator.userAgent.toLowerCase();
     return (
         <>
             <Modal
@@ -17,10 +17,26 @@ const LoginModal = ({ close }, props) => {
             >
                 <ModalContent>
                     <Img src={logo} alt="login" />
-                    <A href={GOOGLE_AUTH_URL} google>
-                        <SocialLogo src={google} />
-                        <Span>구글 계정으로 시작하기</Span>
-                    </A>
+                    {ua.indexOf("kakao") > -1 ? (
+                        <A
+                            onClick={() => {
+                                Swal.fire({
+                                    text: `카카오내에서 구글 로그인은\n할 수 없어요 🥺 `,
+                                    confirmButtonColor: "#7F58EC",
+                                    confirmButtonText: "확인",
+                                });
+                            }}
+                            google
+                        >
+                            <SocialLogo src={google} />
+                            <Span>구글 계정으로 시작하기</Span>
+                        </A>
+                    ) : (
+                        <A href={GOOGLE_AUTH_URL} google>
+                            <SocialLogo src={google} />
+                            <Span>구글 계정으로 시작하기</Span>
+                        </A>
+                    )}
                     <A href={NAVER_AUTH_URL} naver>
                         <SocialLogo src={naver} />
                         <Span>네이버 계정으로 시작하기</Span>
@@ -29,16 +45,6 @@ const LoginModal = ({ close }, props) => {
                         <SocialLogo src={kakao} />
                         <Span kakao>카카오 계정으로 시작하기</Span>
                     </A>
-                    <PDiv>
-                        <P>
-                            구글 로그인은 <Color>인앱 브라우저(카카오톡 등)</Color>에서 지원되지
-                            <br /> 않습니다. 오류 발생시 더보기 &#40; <img src={more} alt="more" />,
-                            &nbsp;
-                            <InfoImg src={share} alt="share" />
-                            &#41;를 눌러서 <Color>다른 브라우저(Chrome, Safari 등)</Color>에서
-                            실행해주세요.
-                        </P>
-                    </PDiv>
                 </ModalContent>
             </Modal>
 
@@ -54,7 +60,7 @@ const LoginModal = ({ close }, props) => {
 const Img = styled.img`
     display: block;
     width: 200px;
-    margin: 44px auto;
+    margin: 56px auto;
 `;
 
 const Back = styled.div`
@@ -90,11 +96,11 @@ const Modal = styled.div`
 
 const ModalContent = styled.div`
     background: #fefefe;
-    margin: 20% auto;
+    margin: 15% auto 20% auto;
     padding: 20px;
     border-radius: 15px;
     max-width: 420px;
-    height: 400px;
+    height: 360px;
     overflow: auto;
     opacity: 1;
     z-index: 99;
@@ -127,27 +133,6 @@ const Span = styled.span`
     font-size: 15px;
     font-weight: 400;
     color: ${(props) => (props.kakao ? "#3C1E20" : "#fff")};
-`;
-
-const PDiv = styled.div`
-    margin: 0 auto;
-    width: 316px;
-`;
-
-const P = styled.p`
-    font-weight: 400;
-    font-size: 12px;
-    margin-top: 36px;
-`;
-
-const Color = styled.p`
-    color: #7f58ec;
-    display: inline;
-`;
-
-const InfoImg = styled.img`
-    width: 14px;
-    margin-top: -4px;
 `;
 
 export default LoginModal;
