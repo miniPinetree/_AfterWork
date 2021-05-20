9f01366061bc87e2b465da69c464df2f3057b835
noh-yj <goky456@gmail.com>
2021-05-20 10:13:51 +0900

    [fix]: 인기추천상품 갯수 수정

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -101,7 +101,7 @@ const getPopularListDB = () => {
   return function (dispatch) {
     axios({
       method: 'get',
-      url: `${config.api}/api/search?page=0&size=24&sort=popularity&direction=desc`,
+      url: `${config.api}/api/search?page=0&size=20&sort=popularity&direction=desc`,
     })
       .then((res) => {
         dispatch(popularList(res.data.content));
