ddd880dfd35c17141861b0eb554bf68e6f69f0c9
go <misong321@gmail.com>
2021-05-21 11:18:07 +0900

    [fix]:Change display when the selected value does not exist

--- src/components/OffTimePicker.js
+++ src/components/OffTimePicker.js
@@ -4,59 +4,57 @@ import { TimePicker } from "antd";
 import { range } from "lodash";
 import moment from "moment";
 
-const OffTimePicker=(props)=>{
+const OffTimePicker = (props) => {
   const user = useSelector((state) => state.user.user);
-  const { setTime, time } =props;
+  const { setTime, time } = props;
+  
+  React.useEffect(() => {
+    if (time === "" && user.offTime) {
+      setTime(user.offTime);
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-React.useEffect(() => {
-  if(time==='' && user.offTime){
-    setTime(user.offTime);
-  };
-}, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  return(
+  return (
     <>
-  {user.offTime?
-  <TimePicker
-  onChange={(time, timeString) => {
-    setTime(timeString);
-  }}
-  size="large"
-  defaultOpenValue={moment(time, "HH:mm:ss")}
-  defaultValue={moment(time, "HH:mm:ss")}
-  value={moment(time, "HH:mm:ss")}
-  hideDisabledOptions={true}
-  disabledHours={() => range(0, 9)}
-  minuteStep={10}
-  secondStep={60}
-  showNow={false}
-  autoFocus={true}
-  onSelect={(value) => {
-    const timeString = moment(value).format("HH:mm:ss");
-    setTime(timeString);
-    console.log(timeString);
-  }}
-/>
-:
-<TimePicker
- onChange={(time, timeString) => {
-  setTime(timeString);
-}}
-  size="large"
-  value={moment(time? time:"00:00:00", "HH:mm:ss")}
-  hideDisabledOptions={true}
-  disabledHours={() => range(0, 9)}
-  minuteStep={10}
-  secondStep={60}
-  showNow={false}
-  autoFocus={true}
-  onSelect={(value) => {
-    const timeString = moment(value).format("HH:mm:ss");
-    setTime(timeString);
-  }}
-/>
-}
-  </>);
+      {user.offTime ? (
+        <TimePicker
+          onChange={(time, timeString) => {
+            setTime(timeString);
+          }}
+          size="large"
+          defaultValue={moment(time, "HH:mm:ss")}
+          value={time ? moment(time, "HH:mm:ss") : undefined}
+          hideDisabledOptions={true}
+          disabledHours={() => range(0, 9)}
+          minuteStep={10}
+          secondStep={60}
+          showNow={false}
+          autoFocus={true}
+          onSelect={(value) => {
+            const timeString = moment(value).format("HH:mm:ss");
+            setTime(timeString);
+          }}
+        />
+      ) : (
+        <TimePicker
+          onChange={(time, timeString) => {
+            setTime(timeString);
+          }}
+          size="large"
+          value={time ? moment(time, "HH:mm:ss") : undefined}
+          hideDisabledOptions={true}
+          disabledHours={() => range(0, 9)}
+          minuteStep={10}
+          secondStep={60}
+          showNow={false}
+          autoFocus={true}
+          onSelect={(value) => {
+            const timeString = moment(value).format("HH:mm:ss");
+            setTime(timeString);
+          }}
+        />
+      )}
+    </>
+  );
 };
 export default React.memo(OffTimePicker);
-
